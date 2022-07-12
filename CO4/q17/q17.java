0010a808d5911b07a8c695098558689778a96838
SANAL MOHAN <70734109+SanalMohan664@users.noreply.github.com>
2022-07-12 22:06:03 +0530

    Update q17.java

--- CO4/q17/q17.java
+++ CO4/q17/q17.java
@@ -4,12 +4,12 @@ public class q17 {
     public static void main(String args[])
     {
         Map<Integer, String> hm2= new HashMap<Integer, String>();
-        hm2.put(new Integer(1), "How");
-        hm2.put(new Integer(2), "are");
-        hm2.put(new Integer(3), "You");
+        hm2.put(new Integer(1), "Hey");
+        hm2.put(new Integer(2), "Hai");
+        hm2.put(new Integer(3), "Hello");
 
         System.out.println(hm2);
-        hm2.put(new Integer(2), "Everyone");
+        hm2.put(new Integer(2), "Hii");
         System.out.println(hm2);
 
         hm2.remove(new Integer(1));
